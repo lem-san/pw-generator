2f2485ea653a2fe771daa126841b124d49abfad0
Raimon <raimon.barragans@gmail.com>
2023-08-21 23:07:39 +0900

    quick changes to advanced options

--- index.js
+++ index.js
@@ -6,22 +6,42 @@ let secondPassword = document.getElementById("pw2")
 
 function generatePassword() {
     let pw1 = ""
-    let pw2 = ""    
-    
-    for (let count = 0; count < 15; count++) {
+    let pw2 = "" 
+
+    let passwordLength = document.getElementById("passwordLength").value
+    let cbSymbols = document.getElementById("cbSymbols")
+    let cbNumbers = document.getElementById("cbNumbers")
+
+    if (cbSymbols.checked === true) {
+      characters.splice(62, characters.length)
+    }
+
+    if (passwordLength === "") {
+      for (let count = 0; count < 15; count++) {
         pw1 += characters[Math.floor(Math.random() * characters.length)]
         pw2 += characters[Math.floor(Math.random() * characters.length)]
+      }
+    } else {
+      for (let count = 0; count < passwordLength; count++) {
+        pw1 += characters[Math.floor(Math.random() * characters.length)]
+        pw2 += characters[Math.floor(Math.random() * characters.length)]
+      }
     }
+
     firstPassword.textContent = pw1
     secondPassword.textContent = pw2
 }
 
 function showOptions(){
-    let options = document.getElementById("optionsSelection");
+    let options = document.getElementById("optionsSelection")
+    let optionsBtn = document.getElementById("optionsBtn")
+
     if (options.style.display === "none") {
-      options.style.display = "flex";
+      options.style.display = "flex"
+      optionsBtn.style.background = "#992831"
     } else {
-      options.style.display = "none";
+      options.style.display = "none"
+      optionsBtn.style.background = "#f3404f"
     }
 }
 
